0817d9c1725089a920991fd41a1e0f087dd39017
Hunter McMillen <mcmillhj@gmail.com>
2016-03-07 17:00:01 -0500

    Initial release of Unicode::CheckUTF8::PP
    Based entirely on Unicode::CheckUTF8, has matching interface but does not require a C compiler

--- README.md
+++ README.md
@@ -10,9 +10,29 @@ Unicode::CheckUTF8::PP - Pure Perl implementation of Unicode::CheckUTF8
 
 version 0.001
 
+# SYNOPSIS
+
+    use Unicode::CheckUTF8::PP qw(is_utf8);
+    my $is_ok = is_utf8($scalar);
+
+# DESCRIPTION 
+
+Pure Perl implementation of [Unicode::CheckUTF8](https://metacpan.org/pod/Unicode::CheckUTF8), almost all logic was directly ported from that module. The target audience of this module are users who would like the functionality of Unicode::CheckUTF8, but don't have access to a C compiler or lack permissions to compile on their systems (for whatever reason)
+
+# METHODS
+
+- `is_utf8`
+
+    Determines whether a Perl scalar is a UTF8 compliant string
+
+        returns 1 if the supplied string is UTF8 compliant 
+        returns 0 otherwise
+
 # AUTHOR
 
 Hunter McMillen <mcmillhj@gmail.com>
+
+Based entirely on [Unicode::CheckUTF8](https://metacpan.org/pod/Unicode::CheckUTF8), written by Brad Fitzpatrick
 
 # COPYRIGHT AND LICENSE
 
--- lib/Unicode/CheckUTF8/PP.pm
+++ lib/Unicode/CheckUTF8/PP.pm
@@ -1,15 +1,131 @@
 package Unicode::CheckUTF8::PP;
+use base qw(Exporter);
 # ABSTRACT: Pure Perl implementation of Unicode::CheckUTF8
 
 use strict;
 use warnings;
 
-sub is_utf8 {
-   ...
-}
+our @EXPORT    = qw();
+our @EXPORT_OK = qw(is_utf8);
 
-sub isLegalUTF8 {
-   ...
+my @validSingleByte = (
+    0,0,0,0,0,0,0,0,0,1,1,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0, 
+    1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+    1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+    1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+    1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0
+);
+ 
+my @trailingBytesForUTF8 = (
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+    2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,3,3,3,3,3,3,3,3,4,4,4,4,5,5,5,5
+);
+
+my $_isLegalUTF8 = sub {
+    my ($bytes, $length) = @_;
+
+    my $i = $length;
+    if ( $length <= 4 ) {
+       my $c;
+       if ( $length == 4 ) {
+          $c = $bytes->[--$i];
+          return 0 unless defined $c;
+          return 0 if $c < 0x80 || $c > 0xBF;
+       }
+      
+       if ( $length >= 3 ) {
+          $c = $bytes->[--$i];    
+          return 0 unless defined $c;
+          return 0 if $c < 0x80 || $c > 0xBF;
+       }
+       
+       if ( $length >= 2 ) {
+          $c = $bytes->[--$i];    
+          return 0 unless defined $c;
+          return 0 if $c > 0xBF;
+          
+          if    ( $bytes->[$i] == 0xE0 ) { return 0 if $c < 0xA0; }
+          elsif ( $bytes->[$i] == 0xF0 ) { return 0 if $c < 0x90; }
+          elsif ( $bytes->[$i] == 0xF0 ) { return 0 if $c > 0x8F; }
+          else                           { return 0 if $c < 0x80; }
+      }
+       
+       if ( $length >= 1 ) {
+          return $validSingleByte[ $bytes->[0] ];
+       }
+    }
+    else {
+       return 0;
+    }
+
+    return 1;
+};
+
+my $_isLegalUTF8String = sub {
+    my ($str) = @_;
+
+    my @bytes = unpack 'U*', $str;
+    my $len = @bytes;
+    my $l = 0;
+    
+    my $i = 0;
+    while ( $i < $len ) {
+       my $byte = $bytes[$i];
+       my $length = $trailingBytesForUTF8[$byte] + 1;
+       
+       # check for early termination of string
+       foreach my $j ( 1 .. $length - 1 ) {
+          return 0 unless defined $bytes[$j];
+          return 0 if $bytes[$j] == 0;
+       }
+       
+       return 0 unless
+          $_isLegalUTF8->([@bytes[$i..$i+$length-1]], $length);
+       
+       $l = $bytes[$i];
+       $i += $length;
+    }
+
+    return $l == $bytes[-1] ? 1 : 0;
+};
+
+sub is_utf8 {
+    return $_isLegalUTF8String->(@_);
 }
 
 1;
+
+=pod 
+
+=head1 DESCRIPTION 
+
+Pure Perl implementation of L<Unicode::CheckUTF8>, almost all logic was directly ported from that module. The target audience of this module are users who would like the functionality of Unicode::CheckUTF8, but don't have access to a C compiler or lack permissions to compile on their systems (for whatever reason)
+
+=head1 SYNOPSIS
+
+ use Unicode::CheckUTF8::PP qw(is_utf8);
+ my $is_ok = is_utf8($scalar);
+
+=head1 METHODS
+
+=over 4
+
+=item C<is_utf8>
+
+Determines whether a Perl scalar is a UTF8 compliant string
+
+ returns 1 if the supplied string is UTF8 compliant 
+ returns 0 otherwise
+
+=back 
+
+=cut
